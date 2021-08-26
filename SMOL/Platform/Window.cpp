6e6b06902526d2292ebeed24b8abca1136efaa78
SweetCelestia <mr.prototype2@mail.ru>
2021-08-27 00:45:26 +0700

    Fixed build

--- SMOL/Platform/Window.cpp
+++ SMOL/Platform/Window.cpp
@@ -1,6 +1,10 @@
 #include "pc.h"
 #include "Window.h"
 
+s32 Window::width = 480;
+s32 Window::height = 272;
+bool Window::windowed = true;
+
 #if SMOL_OS == SMOL_OS_WIN
     #include "Win32/WinWindow.inl"
 #elif SMOL_OS == SMOL_OS_PSP
